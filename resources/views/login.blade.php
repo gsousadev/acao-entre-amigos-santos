2148a2c676d4fb61cfc40ef4be0cdb6c2783b9b2
Guilherme Santos <gsousadev@gmail.com>
2023-09-27 20:54:11 -0300

    fix: Alterando tipo de input de senha para password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
         <form action="/login" method="post">
             @csrf
             <div class="mb-3">
-              <input type="text" class="form-control" name="senha" id="senha" aria-describedby="senha" placeholder="Senha">
+              <input type="password" class="form-control" name="senha" id="senha" aria-describedby="senha" placeholder="Senha">
             </div>
             <button type="submit" class="btn btn-primary">Entrar</button>
         </form>
